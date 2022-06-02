af96d424828f8d6d6cd6dd1ed4c22b3cdd618724
TP00214906 <gulati.parth98@gmail.com>
2022-06-03 02:07:53 +0530

    Created the Container and Content tag

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}>
+          </Route>
+        </Routes>
+        </Router>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const Login = (props) => {
+  <Container>
+      <Content>
+          Content
+      </Content>
+  </Container>
+};
+
+const Container = styled.section`
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
+`
+
+const Content = styled.div`
+    margin-bottom: 10vw;
+    width: 100%;
+    position: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+
+`
+
+export default Login;
