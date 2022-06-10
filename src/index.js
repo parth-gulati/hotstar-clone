32c0f5721010f2a04a1e2a934cdcab77ff2f22cd
Pattypan <gulati.parth98@gmail.com>
2022-06-11 04:18:56 +0530

    Started working on the redux portion of the app, created a userSlice

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './fonts/Avenir-Roman.otf';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={store}>
     <App />
+    </Provider>
 );
